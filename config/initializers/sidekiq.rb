ae793e95bacfa3b425ed60314d1cf1ff59da455f
Rahul Gautam <rahulgautam297@gmail.com>
2015-12-27 23:56:32 +0000

    delete sidekiq files not needed

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: REDISCLOUD_URL, size: 27 }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: REDISCLOUD_URL, size: 3 }
-end
-
-#remove url key to run server locally.
