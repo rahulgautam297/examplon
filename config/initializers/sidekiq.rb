53e539ed7abdf3d7f62367b7f49813e1d44a8fc9
Rahul Gautam <rahulgautam297@gmail.com>
2015-12-27 21:46:45 +0000

    add sidekiq file for heroku

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+require 'sidekiq'
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDISTOGO_URL"] }
+end unless ENV['REDISTOGO_URL'].blank?
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDISTOGO_URL"] }
+end unless ENV['REDISTOGO_URL'].blank?
